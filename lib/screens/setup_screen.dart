f8527528b6784109c4a966ffdba157a2501ea72c
R4XGAMEZ <exeitachi824@gmail.com>
2026-05-02 09:30:31 +0530

    fix: navigationBarColor, InstanceModel import, assets folders

--- lib/screens/setup_screen.dart
+++ lib/screens/setup_screen.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../services/app_state.dart';
+import '../models/instance_model.dart';
 
 class SetupScreen extends StatefulWidget {
   const SetupScreen({super.key});
